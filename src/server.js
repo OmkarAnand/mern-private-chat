9c68eaedac40d2a04dece08568291b96f7d5b581
OmkarAnand <tutu.823204@gmail.com>
2025-10-19 12:22:30 +0530

    Changes in code

--- src/server.js
+++ src/server.js
@@ -20,10 +20,11 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) callback(null, true);
-    else callback(new Error('Not allowed by CORS'));
-  },
+  // origin: function(origin, callback) {
+  //   if (!origin || allowedOrigins.includes(origin)) callback(null, true);
+  //   else callback(new Error('Not allowed by CORS'));
+  // },
+  origin:allowedOrigins,
   methods: ["GET","POST","PUT","DELETE"],
   credentials: true
 }));
